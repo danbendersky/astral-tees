5e2ccfd81012364877940582b041e8361320e524
danbendersky <dbendersky@umass.edu>
2025-06-08 09:55:58 -0400

    initial setup

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
